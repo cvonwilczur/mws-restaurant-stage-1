d2b05fb06d27296aa3ac9454e46e1a0caf62dafe
cvonwilczur <cvonwilczur@gmail.com>
2018-06-22 12:59:54 -0700

    Correction on sw.js

--- sw.js
+++ sw.js
@@ -29,15 +29,6 @@ self.addEventListener('install', function(event) {
   );
 });
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.open('CACHE_NAME').then(function(cache){
-//       return caches.match(event.request).then(function(response){
-//         return response || fetch(event.request);
-//       })
-//     });
-//   );
-// });
 
 self.addEventListener("fetch", function (event) {
   console.log("Request", event.request.url);
@@ -46,17 +37,5 @@ self.addEventListener("fetch", function (event) {
     .then(function(response) {
       return response || fetch(event.request);
     })
-    .catch(function(error) {
-      console.error("Error: ", error);
-    })
   );
 });
-
-
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//       caches.match(event.request).then(function(response) {
-//           return response || fetch(event.request);
-//       })
-//   );
-// });
